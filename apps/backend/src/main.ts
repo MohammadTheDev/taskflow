c215fe756464ef7a54f01c23c3072fefb7ddbe3c
Mohammad Arab <mohammad.arab.dev@gmail.com>
2026-07-17 09:09:43 -0700

    feat(core): configure application bootstrap

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,31 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  app.enableShutdownHooks();
+
+  const port = configService.get<number>('PORT') ?? 3001;
+
+  await app.listen(port);
+
+  console.log(`Server running on http://localhost:${port}/api`)
 }
 bootstrap();
