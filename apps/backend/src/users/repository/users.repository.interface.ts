6f1f021bd514ed951caa1a14c7bde9113e4b10d0
Mohammad Arab <mohammad.arab.dev@gmail.com>
2026-07-18 11:08:31 -0700

    feat(users): implement repository CRUD operations

--- apps/backend/src/users/repository/users.repository.interface.ts
+++ apps/backend/src/users/repository/users.repository.interface.ts
@@ -1,3 +1,15 @@
+import { Prisma } from '../../../generated/prisma/client';
+
 export interface IUsersRepository {
+  findById(id: string);
+
   findByEmail(email: string);
+
+  findMany();
+
+  create(data: Prisma.UserCreateInput);
+
+  update(id: string, data: Prisma.UserUpdateInput);
+
+  delete(id: string);
 }
--- apps/backend/src/users/repository/users.repository.ts
+++ apps/backend/src/users/repository/users.repository.ts
@@ -1,16 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { IUsersRepository } from './users.repository.interface';
+import { Prisma } from '../../../generated/prisma/client';
 
 @Injectable()
 export class UsersRepository implements IUsersRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  async findById(id: string) {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
 
   async findByEmail(email: string) {
     return this.prisma.user.findUnique({
       where: {
         email,
       },
+    });
+  }
+
+  async findMany() {
+    return this.prisma.user.findMany();
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async update(id: string, data: Prisma.UserUpdateInput) {
+    return this.prisma.user.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async delete(id: string) {
+    return this.prisma.user.delete({
+      where: { id },
     });
   }
 }
